772e3f729f20f9c7f322a0ff22b0ad4913d3ebc6
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2021-11-01 16:15:04 +0200

    store script in github

---
+++ training_script.py
@@ -0,0 +1,191 @@
+from tensorflow.python import keras
+from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.layers.core import Lambda
+from tensorflow.python.keras.layers.normalization.batch_normalization import BatchNormalization
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import LSTM
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from sklearn.neighbors import DistanceMetric
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+import nltk
+from nltk.corpus import stopwords
+#nltk.download("wordnet")
+#nltk.download("stopwords")
+
+import zipfile
+import string
+
+import pandas as pd
+import numpy as np
+
+
+
+def read_book (zipfile):
+    print(zipfile.namelist())
+    return zipfile.read(zipfile.namelist()[0]).decode("utf-8")
+
+def load_zipfile_to_memory (fp: str):
+    with zipfile.ZipFile(fp, "r") as zfile:
+        return read_book(zfile)
+
+tbl = str.maketrans("","",string.punctuation+string.digits)
+
+def preprocess_text (text):
+	text = text.translate(tbl)
+	stoppers = stopwords.words("english")
+	text = [w.lower() for w in text.split()]
+	text = [w for w in text if w not in stoppers]
+	text = [w for w in text if w not in ENGLISH_STOP_WORDS]
+	lm = nltk.wordnet.WordNetLemmatizer()
+	text = [lm.lemmatize(w) for w in text]
+	return " ".join(text)
+
+vectorizer = TfidfVectorizer(input="content", use_idf=True)
+
+def calc_tfidf (wordlist):
+    X = vectorizer.fit_transform([wordlist])
+    print(X.shape)
+    df = pd.DataFrame(X[0].T.todense(), index=vectorizer.get_feature_names(), columns=['TF-IDF'])
+    return df
+
+def build_siamese_network (input_shape, embedding_matrix, embedding_dim, max_words, lstm_units):
+	a = Input(shape=input_shape)
+	b = Input(shape=input_shape)
+	emb = Embedding(
+		max_words,
+		embedding_dim,
+		input_length=input_shape[0],
+		weights=[embedding_matrix],
+		trainable=False
+	)
+	lstm = Bidirectional(LSTM(
+			units=lstm_units,
+			dropout=0.2,
+			recurrent_dropout=0.2
+		)
+	)
+	e1 = emb(a)
+	e2 = emb(b)
+	x1 = lstm(e1)
+	x2 = lstm(e2)
+	manhattan_distance = lambda x: keras.backend.abs(x[0]-x[1])
+	merge = Lambda(function=manhattan_distance, output_shape=lambda x: x[0])([x1, x2])
+	#merge = BatchNormalization()(merge)
+	outputs = Dense(1, activation="sigmoid")(merge)
+	model = Model(inputs=[a,b], outputs=outputs)
+	model.summary()
+	model.compile(loss="binary_crossentropy", optimizer="nadam", metrics=["accuracy"])
+	return model
+
+def build_dataframe():
+	master_df = pd.read_csv("pg_catalog.csv", sep=",")
+	master_df = master_df[(master_df.Language == "en") & (master_df.Type == "Text")]
+	master_df.drop(columns=["Bookshelves", "Subjects", "Language", "Type", "Issued"], inplace=True)
+	master_df.dropna(how="any", inplace=True)
+
+	# due to time constraints, only take a random sample of books:
+	rand_books = master_df.sample(n=100)
+
+	# presume that books are similar if: same LoCC
+	similar = [
+		master_df["LoCC"].str.contains(book[0])
+			for book in rand_books[["LoCC"]].to_numpy()
+	]
+	df1 = pd.DataFrame(columns=["a", "b", "similar"])
+	df1["a"] = rand_books["Text#"]
+	df1["b"] = [ master_df[i].sample().iloc[0]["Text#"] for i in similar ]
+	df1["similar"] = [int(1) for _ in range(len(df1["a"]))]
+	df2 = pd.DataFrame(columns=["a", "b", "similar"])
+	df2["a"] = rand_books["Text#"]
+	df2["b"] = [ master_df[~i].sample().iloc[0]["Text#"] for i in similar ]
+	df2["similar"] = [int(0) for _ in range(len(df2["a"]))]
+
+	df = df1.append(df2, ignore_index=True)
+	return df
+
+def get_embeddings_index():
+	embeddings_index = {}
+	with open("glove.6B.100d.txt", "r") as f:
+		for line in f:
+			values = line.split()
+			word = values[0]
+			coefs = np.asarray(values[1:], dtype="float32")
+			embeddings_index[word] = coefs
+	return embeddings_index
+
+def get_sequences(tokenizer, books: list, sequence_max_length: int):
+	assert(len(books) == 2)
+	combined = " ".join([ preprocess_text(b) for b in books ])
+	tokenizer.fit_on_texts(combined)
+	sequences = tokenizer.texts_to_sequences(combined)
+	sequences = pad_sequences(sequences, maxlen=sequence_max_length, padding="post")
+	return sequences
+
+def get_embedding_matrix(tokenizer, max_words, embedding_dim):
+	embeddings_index = get_embeddings_index()
+	embedding_matrix = np.zeros((max_words, embedding_dim))
+	for word, i in tokenizer.word_index.items():
+		if i < max_words:
+			embedding_vector = embeddings_index.get(word)
+		if embedding_vector is not None:
+			embedding_matrix[i] = embedding_vector
+	return embedding_matrix
+
+# TODO: load books into dataframes and then basically do the following stuff
+
+
+
+
+
+
+
+
+'''
+books = []
+books.append(load_zipfile_to_memory("1-0.zip"))
+books.append(load_zipfile_to_memory("2.zip"))
+max_words = 10000
+embedding_dim = 100
+
+tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
+sequences = get_sequences(tokenizer=tokenizer, books=books, sequence_max_length=300)
+embedding_matrix = get_embedding_matrix(tokenizer=tokenizer, max_words=max_words, embedding_dim=embedding_dim)
+
+model = build_siamese_network (
+	input_shape=(300,),
+	embedding_matrix=embedding_matrix,
+	embedding_dim=embedding_dim,
+	lstm_units=20,
+	max_words=max_words
+)
+
+early_stopping = EarlyStopping(monitor="val_loss", patience=3)
+
+print (sequences)
+
+X = sequences
+y = [0]
+'''
+'''
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2)
+Xtrain, Xval, ytrain, yval = train_test_split(Xtrain, ytrain, test_size=0.25)
+
+history = model.fit (
+	[ Xtrain[0], Xtrain[1] ], ytrain,
+	validation_data=([ Xval[0], Xval[1] ], yval),
+	epochs=10, batch_size=64, shuffle=True,
+	callbacks=[early_stopping]
+)
+'''
+
